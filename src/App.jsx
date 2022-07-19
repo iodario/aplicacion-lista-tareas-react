bc16df75e6547e4938aa73df6355dc297878ed57
Dario Turchi <iodariomdq@outlook.com>
2022-07-19 19:51:53 -0300

    initial commit Ok

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import freecodecamplogo from './imagenes/freecodecamp-logo.png'
+
+function App() {
+
+  return (
+    <div className="aplicacion-tareas">
+      <div className="freecodecamp-logo-contenedor">
+        <img src={freecodecamplogo} alt="freecodecamp logo" className="freecodecamp-logo" />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
