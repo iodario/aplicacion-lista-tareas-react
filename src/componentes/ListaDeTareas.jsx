dbf1fb055ddffda51436d8c0c3803a24b3987120
Dario Turchi <iodariomdq@outlook.com>
2022-08-13 10:55:48 -0300

    componente Tarea Ok

--- src/componentes/ListaDeTareas.jsx
+++ src/componentes/ListaDeTareas.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TareaFormulario from './TareaFormulario';
 import '../estilos/ListaDeTareas.css'
+import Tarea from './Tarea.jsx'
 
 function ListaDeTareas() {
-    return(
+    // este componente va a tener un estado especifico, que va a ser un Array
+    // con las tareas que hemos agregado hasta ese mommento
+    // para ello utiizamos un hook, llamado useState
+    // const [estado que queremos mantener , una funcion que utilizamos para actualizar ese estado]
+    // [] valor inicial de tareas
+    // como mostrar una lista de componentes renderizados 
+    // creamos un arreglo de objetos
+
+    const [tareas, setTareas] = useState([]);
+
+
+    return (
         <>
-        <TareaFormulario/>
-        <div className="tareas-lista-contenedor">
-            LISTA DE TAREAS
-        </div>
+            <TareaFormulario />
+            <div className="tareas-lista-contenedor">
+                {
+                    tareas.map((tarea) => // cada tarea se va a representar como un objeto en el arreglo tareas, y para cada uno de ellos vamos a crear un componente <Tarea/>
+                        <Tarea
+                            texto={tarea.texto}
+                            completada={tarea.completada}
+                        />
+                    )
+                }
+            </div>
         </>
     );
+
 
 
 }
--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import '../estilos/Tarea.css'
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tarea({ texto, completada }) {
+function Tarea({ id, texto, completada, completarTarea, eliminarTarea }) {
     return (
         <div className={ completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
-            <div className="tarea-texto">
+            <div 
+                className="tarea-texto"
+                onClick={() => completarTarea(id)}>
                 {texto}
             </div>
-            <div className="tarea-contenedor-iconos"> 
+            <div className="tarea-contenedor-iconos"
+                onClick={() => eliminarTarea(id)}>
                <AiOutlineCloseCircle className='tarea-icono'/> 
             </div>
             
@@ -24,3 +27,9 @@ export default Tarea;
 // {completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}
 // aqui utilizamos un op ternario para asignar, si completada es true DOS clases, si es false UNA clase
 
+//tomamos un id como identificador de la tarea
+//tenemos un texto, que es lo que se va a mostrar
+//tenemos un condicional completada
+//completarTarea es una funcion que nos permite pasar la tarea como completada
+//eliminarTarea es una funcion que nos permite eliminar la tarea
+// de esta forma el componente Tarea NOS QUEDA CON 5 PROPS
